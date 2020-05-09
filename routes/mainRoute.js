01a6bc6b1c7d717c45862ea4e148b88e568e6291
EC2 Default User <ec2-user@ip-172-31-41-25.ec2.internal>
2020-05-09 11:51:55 +0000

    Added Node-Mailer and Quote Page

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -6,5 +6,37 @@ app.get("/", (req, res)=>{
     res.render("index");
 })
 
+app.post('/contact', function (req, res) {
+  var transporter = nodemailer.createTransport({
+     host: 'smtp.gmail.com',
+     port: 465,
+     secure: true, 
+     auth: {
+            user: 'jatinderbirditech@gmail.com',
+            pass: 'Kiranbirdi1@'
+        }
+    });
+    
+    const mailOptions = {
+        from: req.body.email,
+        to: 'jatinderbirditech@gmail.com',
+        subject: 'Contact from GGS AutoCollision',
+        html: `Name: ${req.body.name} <br>
+               Phone: ${req.body.email} <br>
+               Email: ${req.body.message} <br>
+        `
+    };
+    
+    transporter.sendMail(mailOptions, function (err, info) {
+       if(err){
+           console.log(err);
+           res.send("<h1 style='text-align:center'> Error Send Mail </h1> <a href='/'><button>Go Back</button></a>")
+       }
+       else {
+         console.log(info);
+         res.send("Mail has been sent")
+       }
+    });
+});
 
 module.exports = app;
